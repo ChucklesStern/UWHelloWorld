90c245d6e04fb66739c5bbf3e2535c8dd572da5b
ChuccklesStern <Chuckstern@mac.com>
2016-10-13 12:32:54 -0700

    -updated with debug logs and sendEmail intent

--- app/src/main/java/com/example/android/helloworld/MainActivity.java
+++ app/src/main/java/com/example/android/helloworld/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.android.helloworld;
 
-import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,19 +17,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.w("MainActivity", "onCreate");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.mailWidget);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Log.i("Send email", "");
+                    sendEmail();
+
             }
         });
     }
+
+    protected void sendEmail() {
+
+        String[] TO = {""};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email.", "");
+        }
+        catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    public void sendMessage(View view) {
+    public void sendMessage (View view){
+    Log.e("Hey!", "Button Pressed!");
         //This syntax is useful for debugging as it enables us to acquire ID of the clicked object
         String showId = view.getResources().getResourceName(view.getId());
         Toast.makeText(MainActivity.this, showId, Toast.LENGTH_SHORT).show();
@@ -50,6 +75,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "OnStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "OnStop");
+    }
+//seems as if onDestroy does not trigger a breakpoint at all. Why??
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("MainActivity", "OnDestroy");
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
